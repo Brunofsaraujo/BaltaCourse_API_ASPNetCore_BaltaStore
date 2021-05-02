2a15437fbee07473af8dbb4b2a618d45784b38e0
bruno <brunofsaraujo@hotmail.com>
2021-05-02 20:24:09 -0300

    API: Teste de handlers

---
+++ BaltaStore.Tests/Handlers/CustomerHandlerTests.cs
@@ -0,0 +1,32 @@
+using BaltaStore.Domain.StoreContext.Commands.CustomerCommands.Inputs;
+using BaltaStore.Domain.StoreContext.Handlers;
+using BaltaStore.Tests.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaltaStore.Tests.Handlers
+{
+    [TestClass]
+    public class CustomerHandlerTests
+    {
+        [TestMethod]
+        public void ShouldRegisterCustomerWhenCommandIsValid()
+        {
+            var command = new CreateCustomerCommand
+            {
+                FirstName = "Bruno",
+                LastName = "Araujo",
+                Document = "94139093005",
+                Email = "teste@hotmail.com",
+                Phone = "19988789898"
+            };
+
+            Assert.AreEqual(true, command.Valid());
+
+            var handler = new CustomerHandler(new FakeCustomerRepository(), new FakeEmailService());
+            var result = handler.Handle(command);
+
+            Assert.AreNotEqual(null, result);
+            Assert.AreEqual(true, handler.Valid);
+        }
+    }
+}
